e99c5c63a545f6f679b300f672ebda71f955b298
AirXb <1471875497@example.com>
2017-11-12 22:22:56 +0800

    添加了RestController

--- src/main/java/com/osm/antievil/Controller/SearchController.java
+++ src/main/java/com/osm/antievil/Controller/SearchController.java
@@ -2,12 +2,14 @@ package com.osm.antievil.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author airxiao
  * @DESCRIPTION
  * @create 2017 - 11 - 12 20:59
  */
+@RestController
 public class SearchController {
 
     @GetMapping(value = "/hello")
