914b2babc6cc601c4b4201f5accc0fbed7b817d9
wellCh4n <wellch4n@foxmail.com>
2017-11-12 19:42:10 +0800

    Initial commit

---
+++ src/main/java/com/osm/antievil/AntievilApplication.java
@@ -0,0 +1,12 @@
+package com.osm.antievil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntievilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntievilApplication.class, args);
+	}
+}
