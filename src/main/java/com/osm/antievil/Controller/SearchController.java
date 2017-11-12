3a5e39f147bfa0e2b414f1f2d76978c499b5ed34
AirXb <1471875497@example.com>
2017-11-12 21:39:09 +0800

    test

--- src/main/java/com/osm/antievil/Controller/SearchController.java
+++ src/main/java/com/osm/antievil/Controller/SearchController.java
@@ -1,9 +1,17 @@
 package com.osm.antievil.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
- * @author wellCh4n
+ * @author airxiao
  * @DESCRIPTION
  * @create 2017 - 11 - 12 20:59
  */
 public class SearchController {
+
+    @GetMapping(value = "/hello")
+    public String hello(){
+        return "hello world";
+    }
 }
