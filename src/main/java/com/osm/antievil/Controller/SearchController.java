071b0e77f78a2c4b648663491a65032aca9d8f9d
AirXb <1471875497@example.com>
2017-11-13 17:18:24 +0800

    修改了pom.xml中MySQL的version，和配置文件中的数据库信息，按名称查询功能有问题

--- src/main/java/com/osm/antievil/Controller/SearchController.java
+++ src/main/java/com/osm/antievil/Controller/SearchController.java
@@ -3,7 +3,10 @@ package com.osm.antievil.Controller;
 import com.osm.antievil.Entity.Company;
 import com.osm.antievil.Repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,9 +22,22 @@ public class SearchController {
     @Autowired
     private CompanyRepository companyRepository;
 
-    @GetMapping(value = "/hello")
-    public List<Company> hello(){
+    @GetMapping(value = "/searchAll")
+    public List<Company> search(){
         return companyRepository.findAll();
+    }
+
+    @GetMapping(value = "findByName")
+    public List<Company> girlListByName(@RequestParam("name") String name){
+        System.out.println(name);
+        List<Company> c=companyRepository.findByNameLike(name);
+        //System.out.println(a.get(0).getName());
+        return c;
+    }
+
+    @GetMapping(value = "test")
+    public String girlListByAge(){
+        return companyRepository.findAll().get(1).getName();
     }
 
 }
--- src/main/java/com/osm/antievil/Repository/CompanyRepository.java
+++ src/main/java/com/osm/antievil/Repository/CompanyRepository.java
@@ -3,5 +3,9 @@ package com.osm.antievil.Repository;
 import com.osm.antievil.Entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CompanyRepository extends JpaRepository<Company,Integer> {
+    //通过公司名称来查询
+    public List<Company> findByNameLike(String name);
 }
