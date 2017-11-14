9cb4305ff01ebe58dfd5a09540d9741066aa5ca1
AirXb <1471875497@example.com>
2017-11-14 19:27:56 +0800

    添加了相似度查询功能

--- src/main/java/com/osm/antievil/Controller/SearchController.java
+++ src/main/java/com/osm/antievil/Controller/SearchController.java
@@ -2,18 +2,16 @@ package com.osm.antievil.Controller;
 
 import com.osm.antievil.Entity.Company;
 import com.osm.antievil.Repository.CompanyRepository;
-import com.osm.antievil.Tools.PinyinQuery;
+import com.osm.antievil.Tools.CompareString;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.osm.antievil.Tools.PinyinQuery.changeToPinyin;
-import static com.osm.antievil.Tools.PinyinQuery.getPinyin;
 
 /**
  * @author airxiao
@@ -45,6 +43,29 @@ public class SearchController {
         //System.out.println(name);
         String newName=changeToPinyin(name);
         return companyRepository.findByPinyinnameLike(newName);
+    }
+
+    @GetMapping(value = "/findVague")
+    public List<Company> companyListVague(@RequestParam("name") String name,@RequestParam("area") String area){
+        CompareString compareString = new CompareString();
+        String newName=changeToPinyin(name);
+        String newArea=changeToPinyin(area);
+        //查询所有结果
+        List<Company> allResult =  companyRepository.findAll();
+        List<Company> returnResult= new ArrayList<Company>();
+        //循环遍历出它们的pinyinname
+        //用输入的name去比较
+        //相似度大于0.7,就存下来
+        for (Company c:allResult) {
+
+            float result= compareString.getSimilarityRatio(newName, c.getPinyinname());
+            if(result>0.3){
+                System.out.println(c.getName()+"---"+result);
+                returnResult.add(c);
+            }
+        }
+
+        return returnResult;
     }
 
 
